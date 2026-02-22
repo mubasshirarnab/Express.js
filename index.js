5e2d3337a301d7e44842066a50e71afbb02d98f8
unknown <arnab0574@gmail.com>
2026-02-23 03:16:13 +0600

    Define a route handler for POST requests to '/data'

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+// Define a route handler for the root URL ("/")
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
+
+// Define a route handler for POST requests to "/data"
+app.post('/data', (req, res) => {
+    const data = req.body;
+    console.log('Received data:', data);
+    res.send('Data received successfully!');
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
